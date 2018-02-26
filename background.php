b0f1991ee8d9355296538d1817ed73f6124cd1b4
Halim Tuhu Prasetyo <halimtuhuprasetyo@gmail.com>
2018-02-26 18:01:11 +0700

    perubahan di background

--- background.php
+++ background.php
@@ -1,11 +1,10 @@
 <section id="about" class="section-bg">
   <div class="container-fluid">
     <div class="section-header">
-      <h3 class="section-title">About Us</h3>
+      <h3 class="section-title">About KNB</h3>
       <span class="section-divider"></span>
       <p class="section-description">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque<br>
-        sunt in culpa qui officia deserunt mollit anim id est laborum
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </div>
 
