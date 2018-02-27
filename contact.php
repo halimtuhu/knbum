b1ffbc70d5132d9f463a031c50d626c8cde6f398
belabonga <shania1faza@gmail.com>
2018-02-27 18:38:48 +0700

    Contack DONE

--- contact.php
+++ contact.php
@@ -4,14 +4,13 @@
 
       <div class="col-lg-4 col-md-4">
         <div class="contact-about">
-          <h3>Contact Panitia KNB</h3>
-          <p>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+          <h3>Contact</h3>
+          <p>Stay connect with us</p>
           <div class="social-links">
-            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            <a href="http://oia.um.ac.id/" class="website" target="_blank"><i class="fa fa-globe"></i></a>
+            <a href="https://www.facebook.com/internationalaffairsUM" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+            <a href="https://www.instagram.com/oia_um/" class="instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+            <a href="https://www.youtube.com/channel/UC1XP5w9EUyHGsKdZJU-awcQ" class="youtube" target="_blank"><i class="fa fa-youtube-play"></i></a>
           </div>
         </div>
       </div>
@@ -20,47 +19,29 @@
         <div class="info">
           <div>
             <i class="ion-ios-location-outline"></i>
-            <p>A108 Adam Street<br>New York, NY 535022</p>
+            <p>Jalan Semarang 5, Malang<br>Indonesia, 65135</p>
           </div>
 
           <div>
             <i class="ion-ios-email-outline"></i>
-            <p>info@example.com</p>
+            <p>oia@um.ac.id<br>iro@um.ac.id</p>
           </div>
 
           <div>
             <i class="ion-ios-telephone-outline"></i>
-            <p>+1 5589 55488 55s</p>
+            <p>+62 341-584-759<br>+62 341-551312</p>
+          </div>
+
+          <div>
+            <i class="ion-ios-paperplane-outline"></i>
+            <p>+62 341-584-759</p>
           </div>
 
         </div>
       </div>
 
-      <div class="col-lg-5 col-md-8">
-        <div class="form">
-          <div id="sendmessage">Your message has been sent. Thank you!</div>
-          <div id="errormessage"></div>
-          <form action="" method="post" role="form" class="contactForm">
-            <div class="form-row">
-              <div class="form-group col-lg-6">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                <div class="validation"></div>
-              </div>
-              <div class="form-group col-lg-6">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                <div class="validation"></div>
-              </div>
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-              <div class="validation"></div>
-            </div>
-            <div class="form-group">
-              <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-              <div class="validation"></div>
-            </div>
-            <div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
-          </form>
+        <div class="col-lg-5 col-md-8">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.349620280228!2d112.6162893149048!3d-7.9627739942648015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e788281bdd08839%3A0xc915f268bffa831f!2sMalang+State+University!5e0!3m2!1sen!2sid!4v1519730646460" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
       </div>
 
