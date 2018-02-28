b804acf41177e8e983e0b100c9ed8cd78f3b2382
younggarega <younggarega@gmail.com>
2018-02-28 19:38:39 +0700

    Knb done 19.28

--- knb.php
+++ knb.php
@@ -17,7 +17,11 @@
       <div class="col-lg-8 col-md-7 ">
 
         <div class="row">
+          <p>
+            &nbsp; &nbsp; &nbsp;Kemitraan Negara Berkembang <b>“Developing Countries Partnership” (KNB)</b> is a scholarship program offered by the Indonesian Government to potential students from developing countries to earn their <em>Master Degree</em> at one of 16 prominent universities in Indonesia. Universitas Negri Malang (UM), one of the leading universities in Indonesia, has been working with the Indonesian Government to implement and manage the KNB scholarship since 2008. There are 20 KNB grantees on Universitas Negri Malang, origin from various countries: <em>Thailand, Comoro, Timor Leste, Sierra Leonne, Uganda, Papua New Guinea, Tanzania, Madagascar, Mauritius, Palestine.</em>
+          </p>
 
+          <!--
           <div class="col-lg-6 col-md-6 box wow fadeInRight">
             <div class="icon"><i class="ion-ios-speedometer-outline"></i></div>
             <h4 class="title"><a href="">Lorem Ipsum</a></h4>
@@ -28,9 +32,11 @@
             <h4 class="title"><a href="">Dolor Sitema</a></h4>
             <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata noble dynala mark.</p>
           </div>
+          -->
+
           <div class="col-lg-6 col-md-6 box wow fadeInRight" data-wow-delay="0.2s">
             <div class="icon"><i class="ion-social-buffer-outline"></i></div>
-            <h4 class="title"><a href="">Objectives:</a></h4>
+            <h4 class="title"><a href="">Objectives :</a></h4>
             <p class="description">
                 Contributing in human resource development in the developing countries.<br>
                 Promoting deeper cultural understanding among developing countries.<br>
@@ -39,7 +45,7 @@
           </div>
           <div class="col-lg-6 col-md-6 box wow fadeInRight" data-wow-delay="0.3s">
             <div class="icon"><i class="ion-ios-analytics-outline"></i></div>
-            <h4 class="title"><a href="">Scholarship’s Coverage:</a></h4>
+            <h4 class="title"><a href="">Scholarship’s Coverage :</a></h4>
             <p class="description">
                 Accommodation<br>
                 Healtf Insurance<br>
