64d089f2dfb81d3c259d3b6f5070028714ebf6c9
belabonga <shania1faza@gmail.com>
2018-03-01 05:25:32 +0700

    edit dikit (2)

--- contact.php
+++ contact.php
@@ -7,17 +7,10 @@ s<section id="contact">
           <h3>Contact</h3>
           <p>Stay connect with us</p>
           <div class="social-links">
-<<<<<<< Updated upstream
             <a href="http://oia.um.ac.id/" class="website" target="_blank"><i class="fa fa-globe"></i></a>
             <a href="https://www.facebook.com/internationalaffairsUM" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
             <a href="https://www.instagram.com/oia_um/" class="instagram" target="_blank"><i class="fa fa-instagram"></i></a>
             <a href="https://www.youtube.com/channel/UC1XP5w9EUyHGsKdZJU-awcQ" class="youtube" target="_blank"><i class="fa fa-youtube-play"></i></a>
-=======
-            <a href="https://www.facebook.com/internationalaffairsUM" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a>
-            <a href="https://www.instagram.com/oia_um/" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a>
-            <a href="http://oia.um.ac.id/" target="_blank" class="website"><i class="fa fa-globe"></i></a>
-            <a href="https://www.youtube.com/channel/UC1XP5w9EUyHGsKdZJU-awcQ" target="_blank" class="youtube"><i class="fa fa-youtube-play"></i></a>
->>>>>>> Stashed changes
           </div>
         </div>
       </div>
@@ -26,11 +19,7 @@ s<section id="contact">
         <div class="info">
           <div>
             <i class="ion-ios-location-outline"></i>
-<<<<<<< Updated upstream
             <p>Jalan Semarang 5, Malang<br>Indonesia, 65135</p>
-=======
-            <p>Jalan Semarang 5, Malang<br>Indonesia, 65145</p>
->>>>>>> Stashed changes
           </div>
 
           <div>
@@ -41,16 +30,7 @@ s<section id="contact">
           <div>
             <i class="ion-ios-telephone-outline"></i>
             <p>+62 341-584-759<br>+62 341-551312</p>
-<<<<<<< Updated upstream
           </div>
-
-          <div>
-            <i class="ion-ios-paperplane-outline"></i>
-            <p>+62 341-584-759</p>
-=======
->>>>>>> Stashed changes
-          </div>
-
           <div>
             <i class="ion-ios-paperplane-outline"></i>
             <p>+62 341-584-759</p>
@@ -59,15 +39,9 @@ s<section id="contact">
 
         </div>
       </div>
-
-<<<<<<< Updated upstream
         <div class="col-lg-5 col-md-12">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.349620280228!2d112.6162893149048!3d-7.9627739942648015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e788281bdd08839%3A0xc915f268bffa831f!2sMalang+State+University!5e0!3m2!1sen!2sid!4v1519730646460" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
-=======
-      <div class="col-lg-5 col-md-8" class="map">
-            <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.349620280228!2d112.6162893149048!3d-7.9627739942648015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e788281bdd08839%3A0xc915f268bffa831f!2sMalang+State+University!5e0!3m2!1sen!2sid!4v1519730646460" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
->>>>>>> Stashed changes
       </div>
 
     </div>
