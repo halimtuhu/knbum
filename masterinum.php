74765c1cef70674b486c5053e1eed756d34ac512
younggarega <younggarega@gmail.com>
2018-03-12 00:01:21 +0700

    update bachelor degree [Youngga]

--- masterinum.php
+++ masterinum.php
@@ -14,7 +14,7 @@
             <p>
               <table cellspacing="0" align="center" cellpadding="1">
     <tr>
-        <th colspan="3"><center>List of Courses</center></th>
+        <th colspan="3"><center>List of Courses S2</center></th>
     </tr>
     <tr>
         <td data-content="List of Courses">
@@ -57,6 +57,89 @@
             <li>Civil Engineering</li>
             <li>Special Needs Education</li>
             <li>Early Childhood Education</li>
+          </ul>
+        </td>
+    </tr>
+</table>
+            </p>
+              <h2>Bachelor Programs (S1) in Universitas Negeri Malang (UM)</h2>
+            <h3>List of courses provided in Universitas Negeri Malang Bachelor Degree (S1) Program :</h3>
+            <p>
+            <!-- Bisa ditambahkan informasi -->
+            </p>
+            <p>
+              <table cellspacing="0" align="center" cellpadding="1">
+    <tr>
+        <th colspan="3"><center>List of Courses S1</center></th>
+    </tr>
+    <tr>
+        <td data-content="List of Courses">
+            <ul>
+            <li>Educational Administration</li>
+            <li>Accounting</li>
+            <li>Indonesian Language and Literature</li>
+            <li>English Language Education</li>
+            <li>Guidance and Counselling</li>
+            <li>Biology</li>
+            <li>Visual Communication Design</li>
+            <li>Economics and Development Studies</li>
+            <li>Physics</li>
+            <li>Geography</li>
+            <li>Social Science Education</li>
+            <li>Sports Education</li>
+            <li>Sports Training Education</li>
+            <li>Chemistry Education</li>
+            <li>Special and Inclusive Education</li>
+            <li>Non-Formal Education</li>
+            <li>MathematicsEducation</li>
+            <li>Pancasila and Civics Education</li>
+            <li>Psychology</li>
+            <li>History Education</li>
+            </ul>
+        </td>
+        <td data-content="List of Courses">
+          <ul>
+            <li>Sports Science</li>
+            <li>Public Health Science</li>
+            <li>History</li>
+            <li>Chemistry</li>
+            <li>Management</li>
+            <li>Mathematics</li>
+            <li>Office Administration Education</li>
+            <li>Accounting Education</li>
+            <li>Arabic Language Education</li>
+            <li>English Language Education</li>
+            <li>Visual Art Education</li>
+            <li>Dance and Music Education</li>
+            <li>Sociology</li>
+            <li>Culinary Education</li>
+            <li>Fashion Education</li>
+            <li>Business and Management education</li>
+            <li>Civil Engineering Education</li>
+            <li>Electrical Engineering Education</li>
+            <li>Informatics Engineering Education</li>
+          </ul>
+        </td>
+        <td data-content="List of Courses">
+          <ul>
+            <li>German Language Education</li>
+            <li>Mandarin Language Education</li>
+            <li>Indonesian Language Education</li>
+            <li>Biology Education</li>
+            <li>Economics Education</li>
+            <li>Physics Education</li>
+            <li>Geography Education</li>
+            <li>Early Childhood Teacher Education</li>
+            <li>Elementary School Teacher Education</li>
+            <li>Science Education</li>
+            <li>Mechanical Engineering Education</li>
+            <li>Automotive Engineering Education</li>
+            <li>Electrical Engineering</li>
+            <li>Informatics Engineering</li>
+            <li>Mechanical Engineering</li>
+            <li>Civil Engineering</li>
+            <li>Technology Education</li>
+
           </ul>
         </td>
     </tr>
